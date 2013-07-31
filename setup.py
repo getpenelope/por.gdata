f24b6706a687adf5d8f3d3c90a0413c698af7f88
Andrew Mleczko <andrew@mleczko.net>
2013-07-31 10:08:43 +0200

    Back to development: 1.7.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if sys.version_info[:3] < (2,5,0):
     requires.append('pysqlite')
 
 setup(name='por.gdata',
-      version='1.7.2',
+      version='1.7.3.dev0',
       description='Penelope gdata integration',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
